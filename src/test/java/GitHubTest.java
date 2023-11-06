7fd7b2c815858f270feeeed0b2027fefc13e4cf3
Aleksei Korolev <alexkorolev86@gmail.com>
2023-11-06 15:59:34 +0300

    commit 2

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -8,33 +10,24 @@ import static com.codeborne.selenide.Selenide.*;
 public class GitHubTest {
     @BeforeEach
     void beforeMethod(){
-        Configuration.baseUrl = "https://github.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void fillThePracticeForm() {
-        String codeJUnit5 =
-                "@ExtendWith({SoftAssertsExtension.class})\n" +
-                        "class Tests {\n" +
-                        "  @Test\n" +
-                        "  void test() {\n" +
-                        "    Configuration.assertionMode = SOFT;\n" +
-                        "    open(\"page.html\");\n" +
-                        "\n" +
-                        "    $(\"#first\").should(visible).click();\n" +
-                        "    $(\"#second\").should(visible).click();\n" +
-                        "  }\n" +
-                        "}";
-
-        open("/selenide/selenide");
-        $("#wiki-tab").click();
-        $(".wiki-rightbar ul li button").click();
-        $(".wiki-rightbar ul").shouldHave(text("SoftAssertions"));
-        $(byText("SoftAssertions")).click();
-        $(".markdown-body").shouldHave(text(codeJUnit5));
-
-        sleep(3000);
+    void openEnterprise() {
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
     }
+
+    @Test
+    void testDragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+       }
 }
